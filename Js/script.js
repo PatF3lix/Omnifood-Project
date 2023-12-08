93b71fa297efa113969b497b3c855986a5f21931
PatF3lix <pat_pg@hotmail.fr>
2023-12-08 20:42:33 +0000

    implemented sticky navigation

--- Js/script.js
+++ Js/script.js
@@ -71,10 +71,38 @@ const smoothScrolling = function () {
   );
 };
 
+/**Sticky Navigation */
+
+const stickyNav = function () {
+  const sectionHeroEl = document.querySelector(".section-hero");
+
+  const obs = new IntersectionObserver(
+    function (entries) {
+      const ent = entries[0];
+
+      if (!ent.isIntersecting) {
+        document.body.classList.add("sticky");
+      }
+
+      if (ent.isIntersecting) {
+        document.body.classList.remove("sticky");
+      }
+    },
+    {
+      //In the viewport
+      root: null,
+      threshold: 0,
+      rootMargin: "-80px",
+    }
+  );
+  obs.observe(sectionHeroEl);
+};
+
 const init = function () {
   updateYear();
   toggleMobileNavMenu();
   smoothScrolling();
+  stickyNav();
 };
 
 init();
